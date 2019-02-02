910c7f8c79e4b3932b55d10ba4f736695f818fe2
Mudit Mittal <mmudit30@gmail.com>
2019-02-02 22:57:58 +0530

    added checkout class

--- app/src/main/java/com/socialapp/socialhelpapp/CheckoutActivity.java
+++ app/src/main/java/com/socialapp/socialhelpapp/CheckoutActivity.java
@@ -1,4 +1,12 @@
 package com.socialapp.socialhelpapp;
 
-public class CheckoutActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheckoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
 }
